790ee5da998ac41e918ec04eeeaa90fe52e66221
shenyonghe525 <906425726@qq.com>
2015-11-10 15:47:19 +0800

    material-design ripple
    material-design ripple

---
+++ app/src/main/java/com/nav/syh/yongheshen/androidviews/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nav.syh.yongheshen.androidviews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo);
+    }
+}
